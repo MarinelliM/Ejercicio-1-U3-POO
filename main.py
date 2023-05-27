6b6ad4a4ac9e9582bc3ab7d20cb365d1febdc258
MarinelliM <103908022+MarinelliM@users.noreply.github.com>
2023-05-27 14:11:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,5 @@ from Menu import menu
 if __name__ == '__main__':
     Manejador = ManejaFacultad() 
     Manejador.initmf()
-    a = input('ingrese un valor:')
     Manejador.test()
     menu(Manejador)
